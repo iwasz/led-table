d1f8b09c18ed318a9595583cd53fa1f5e0846ba4
Łukasz Iwaszkiewicz <lukasz.iwaszkiewicz@gmail.com>
2020-03-27 11:07:35 +0100

    Very very strange error. Program works unpredictably when \n changed to \r\n in usbh_conf.h. Looks like something were deeply wrong like uninitialized variable?

--- src/stm32/usb/usbh_conf.h
+++ src/stm32/usb/usbh_conf.h
@@ -92,7 +92,7 @@
 #define USBH_ErrLog(...)                                                                                                                        \
         printf ("ERROR: ");                                                                                                                     \
         printf (__VA_ARGS__);                                                                                                                   \
-        printf ("\r\n");
+        printf ("\n");
 #else
 #define USBH_ErrLog(...)
 #endif
